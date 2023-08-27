b9e79b1223bf406f2bce64d1e7a6859b1b0b8406
Shamit Fatin <shamit187@gmail.com>
2023-08-27 18:39:17 +0600

    first commit

---
+++ main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+int main(void)
+{
+    GLFWwindow *window;
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /*
+        YOUR CODE HERE
+        */
+        glBegin(GL_TRIANGLES);
+        glColor3f(1.0f, 0.0f, 0.0f);
+        glVertex3f(-0.6f, -0.4f, 0.0f);
+        glColor3f(0.0f, 1.0f, 0.0f);
+        glVertex3f(0.6f, -0.4f, 0.0f);
+        glColor3f(0.0f, 0.0f, 1.0f);
+        glVertex3f(0.0f, 0.6f, 0.0f);
+        glEnd();
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+    glfwTerminate();
+    return 0;
+}
