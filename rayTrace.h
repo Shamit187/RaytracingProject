60d71290aca815c0dd69428aa2f41202ed451f82
Shamit Fatin <shamit187@gmail.com>
2023-08-31 13:28:10 +0600

    I Think Light Intersection works properly Now

--- rayTrace.h
+++ rayTrace.h
@@ -15,39 +15,41 @@
 const Color ambientColor = Color(0.0f, 0.0f, 0.0f);
 // std::ofstream logFile("log.txt");
 
-bool checkLightVisibility(const vec3 &point, const vec3 &light, int id)
+bool lightVisible(const vec3 &point, const vec3 &lightPosition, const int id)
 {
-    auto ray = Ray(point, point - light);
-    auto intersection = ray.intersectCheckerBoard();
-    if (id != -1 && intersection.valid == true && (ray.intersectCheckerBoard().point - point).length() < (light - point).length())
-        return false;
-
+    Ray ray = Ray(point, (lightPosition - point).normalize());
+    Intersection intersection;
+    GLfloat depth = (lightPosition - point).length();
     for (auto sphere : spheres)
     {
         if (sphere.id == id)
             continue;
-        if (ray.intersect(sphere).valid == true && (ray.intersect(sphere).point - point).length() < (light - point).length())
+        intersection = ray.intersect(sphere);
+        if (intersection.valid == false)
+            continue;
+        if ((intersection.point - point).length() < depth)
             return false;
     }
-
     for (auto triangle : triangles)
     {
         if (triangle.id == id)
             continue;
-        if (ray.intersect(triangle).valid == true && (ray.intersect(triangle).point - point).length() < (light - point).length())
+        intersection = ray.intersect(triangle);
+        if (intersection.valid == false)
+            continue;
+        if ((intersection.point - point).length() < depth)
             return false;
     }
-
     for (auto quad : quads)
     {
-        // if(id == -1)
-        //     logFile << "Quad hit? " << quad.id << "\n";
         if (quad.id == id)
             continue;
-        if (ray.intersect(quad).valid == true && (ray.intersect(quad).point - point).length() < (light - point).length())
+        intersection = ray.intersect(quad);
+        if (intersection.valid == false)
+            continue;
+        if ((intersection.point - point).length() < depth)
             return false;
     }
-
     return true;
 }
 
@@ -297,7 +299,7 @@ Color raycast(const Ray &ray, const int iterationDeapth)
         material.ambient = checkerboardAmbientCoeff;
         material.diffuse = checkerboardDiffuseCoeff;
         material.reflection = checkerboardReflectionCoeff;
-        material.color = ((int)(intersection.point.x / checkerboardWidth) + (int)(intersection.point.z / checkerboardWidth)) % 2 == 0 ? Color(0.0f, 0.0f, 0.0f) : Color(1.0f, 1.0f, 1.0f);
+        material.color = ((int)(intersection.point.x / checkerboardWidth) + (int)(intersection.point.z / checkerboardWidth)) % 2 == 0 ? Color(1.0f, 1.0f, 1.0f) : Color(1.0f, 1.0f, 1.0f);
         material.specular = 0.0f;
         material.shininess = 0;
         tempColor = Color(0.0f, 0.0f, 0.0f);
@@ -310,19 +312,19 @@ Color raycast(const Ray &ray, const int iterationDeapth)
             if (intersection.normal.dot(normalLight.position - intersection.point) < 0)
                 continue;
 
-            if (!checkLightVisibility(intersection.point, normalLight.position, -1))
+            if(!lightVisible(intersection.point, normalLight.position, -1))
                 continue;
             
-            // auto lightDirection = (normalLight.position - intersection.point).normalize();
-            // auto normal = intersection.normal.normalize();
-            // auto lightDistance = (normalLight.position - intersection.point).length();
-            // auto scalingFactor = exp(-lightDistance * lightDistance * normalLight.falloff);
-            // auto lightIntensity = normalLight.intensity * scalingFactor;
+            auto lightDirection = (normalLight.position - intersection.point).normalize();
+            auto normal = intersection.normal.normalize();
+            auto lightDistance = (normalLight.position - intersection.point).length();
+            auto scalingFactor = exp(-lightDistance * lightDistance * normalLight.falloff);
+            auto lightIntensity = normalLight.intensity * scalingFactor;
 
-            // lambert += std::max(0.0f, normal.dot(lightDirection)) * lightIntensity;
-            // auto reflectedRay = (normal * 2.0f * normal.dot(lightDirection) - lightDirection).normalize();
-            // phong += pow(std::max(0.0f, reflectedRay.dot(ray.direction)), material.shininess) * lightIntensity;
-            tempColor = tempColor + normalLight.color * material.color;
+            lambert += std::max(0.0f, normal.dot(lightDirection)) * lightIntensity;
+            auto reflectedRay = (normal * 2.0f * normal.dot(lightDirection) - lightDirection).normalize();
+            phong += pow(std::max(0.0f, reflectedRay.dot(ray.direction)), material.shininess) * lightIntensity;
+            tempColor = tempColor + normalLight.color * material.color * material.diffuse * lambert + normalLight.color * material.color * material.specular * phong;
         }
         for (auto spotLight : spotLights)
         {
@@ -330,7 +332,7 @@ Color raycast(const Ray &ray, const int iterationDeapth)
             if (intersection.normal.dot(spotLight.position - intersection.point) < 0)
                 continue;
 
-            if (!checkLightVisibility(intersection.point, spotLight.position, -1))
+            if(!lightVisible(intersection.point, spotLight.position, -1))
                 continue;
 
             // check if angle with light is less than cutoff
@@ -338,15 +340,15 @@ Color raycast(const Ray &ray, const int iterationDeapth)
             if (lightDirection.dot(spotLight.direction) < cos(spotLight.cutoff))
                 continue;
 
-            // auto normal = intersection.normal.normalize();
-            // auto lightDistance = (spotLight.position - intersection.point).length();
-            // auto scalingFactor = exp(-lightDistance * lightDistance * spotLight.falloff);
-            // auto lightIntensity = spotLight.intensity * scalingFactor;
+            auto normal = intersection.normal.normalize();
+            auto lightDistance = (spotLight.position - intersection.point).length();
+            auto scalingFactor = exp(-lightDistance * lightDistance * spotLight.falloff);
+            auto lightIntensity = spotLight.intensity * scalingFactor;
 
-            // lambert += std::max(0.0f, normal.dot(lightDirection)) * lightIntensity;
-            // auto reflectedRay = (normal * 2.0f * normal.dot(lightDirection) - lightDirection).normalize();
-            // phong += pow(std::max(0.0f, reflectedRay.dot(ray.direction)), material.shininess) * lightIntensity;
-            tempColor = tempColor + spotLight.color * material.color;
+            lambert += std::max(0.0f, normal.dot(lightDirection)) * lightIntensity;
+            auto reflectedRay = (normal * 2.0f * normal.dot(lightDirection) - lightDirection).normalize();
+            phong += pow(std::max(0.0f, reflectedRay.dot(ray.direction)), material.shininess) * lightIntensity;
+            tempColor = tempColor + spotLight.color * material.color * material.diffuse * lambert + spotLight.color * material.color * material.specular * phong;
         }
 
         // tempColor = tempColor + ambientColor * material.ambient + material.color * material.diffuse * lambert + material.color * material.specular * phong;
